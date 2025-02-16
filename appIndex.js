a582901aba9b86e83a1d070cb7904d139a9c848a
WebRabbits <grig5230@gmail.com>
2025-02-16 18:36:19 +0300

    Добавил html-разметку для фичи вставки и замены в тексте любого значения которе передаётся в поле поиска для замены. Добавлены новые стили в файл style.css Добавлены новые функции в appIndex.js: • toggleChangeBlockText() • changeText(); • validChangeText(); • copyChangeText();

--- appIndex.js
+++ appIndex.js
@@ -6,32 +6,58 @@ body.insertAdjacentHTML(
   'afterbegin',
   `
 <div class="mainBlock">
-  <div class="containerBtn">
-  <button type="button" class="resetBtn">Clear all storage</button>
-  <p class="outText-1">Clear all storage DONE!!!</p>
-</div>
+    <div class="containerBtn">
+    <button type="button" class="resetBtn">Clear all storage</button>
+    <p class="outText-1">Clear all storage DONE!!!</p>
+  </div>
 
-<div class="randomIban-block">
-  <select name="" id="country">
-      <option disabled selected="selected">Select country</option>
-      <option value="26">TR - Turkey</option>
-      <option value="16">BE - Belgium</option>
-      <option value="20">AT - Austria</option>
-  </select>
-    <button type="button" class="copy">Copy</button>
-<!--<button class="get-random-iban-btn">Получить IBAN</button>-->
-<p class="result-random-iban-block"></p>
-</div>
-<div class="randomPhone-block" title="Click on phone number to copy">
-  <select name id="randomPhone">
-    <option disabled selected="selected">Select phone</option>
-    <option value=11>Russia +7</option>
-    <option value=11>Belgium +32</option>
-    <option value=12>Brazil +55</option>
-    <option value=12>Turkey +90</option>
-  </select>
-<p class="result-random-phone-block"></p>
-</div>
+  <div class="randomIban-block">
+    <select name="" id="country">
+        <option disabled selected="selected">Select country</option>
+        <option value="26">TR - Turkey</option>
+        <option value="16">BE - Belgium</option>
+        <option value="20">AT - Austria</option>
+    </select>
+      <button type="button" class="copy">Copy</button>
+  <!--<button class="get-random-iban-btn">Получить IBAN</button>-->
+  <p class="result-random-iban-block"></p>
+  </div>
+  <div class="randomPhone-block" title="Click on phone number to copy">
+    <select name id="randomPhone">
+      <option disabled selected="selected">Select phone</option>
+      <option value=11>Russia +7</option>
+      <option value=11>Belgium +32</option>
+      <option value=12>Brazil +55</option>
+      <option value=12>Turkey +90</option>
+    </select>
+  <p class="result-random-phone-block"></p>
+  </div>
+
+  <div class="change-text-block">
+      <button class="change-text-block-btn">Изменить текст ▼</button>
+      <div class="change-container">
+        <div class="block-search">
+          <label>Найти:</label>
+          <input type="text" class="input-search" placeholder="Введите текст для поиска..."/>
+        </div>
+        <div class="block-change">
+          <label>Заменить:</label>
+          <input type="text" class="input-search" placeholder="Введите текст для замены..."/>
+        </div>
+        <div class="error-block-change"></div>
+        <div class="block-change-text">
+          <textarea name="block-text" id="" cols="80" rows="10" placeholder="Введите текст для изменения..."></textarea>
+        </div>
+        <div class="btn-block">
+          <div class="btn-change-block">
+            <button class="btn-change-text">Заменить</button>
+          </div>
+          <div class="btn-copy-block">
+            <button class="btn-copy-text">Копировать</button>
+          </div>
+        </div>
+      </div>
+    </div>
 </div>
 `
 );
@@ -44,13 +70,24 @@ const selectPhone = document.querySelector('#randomPhone');
 const resultBlockPhone = document.querySelector('.result-random-phone-block');
 const btnCopy = document.querySelector('.copy');
 
-// Созадём ивенты
+const changeTextBlock = document.querySelector('.change-text-block');
+const changeTextBlockBtn = document.querySelector('.change-text-block-btn');
+const inputSearch = document.querySelectorAll('.input-search');
+const blockText = document.querySelector('textarea[name="block-text"]');
+const changeBtn = document.querySelector('.btn-change-text');
+const copyBtn = document.querySelector('.btn-copy-text');
+
+// Создаём ивенты
 resetBtn.addEventListener('click', handleClearAllStorage);
 selectCountry.addEventListener('change', getCountryData);
 btnCopy.addEventListener('click', doCopy);
 
 selectPhone.addEventListener('change', getPhoneData);
 resultBlockPhone.addEventListener('click', copyPhone);
+
+changeTextBlockBtn.addEventListener('click', toggleChangeBlockText);
+changeBtn.addEventListener('click', () => changeText(validChangeText));
+copyBtn.addEventListener('click', copyChangeText);
 
 /*-------------------------------------------------------------------------------------------------------*/
 
@@ -70,7 +107,7 @@ function handleClearAllStorage() {
   notificationDone();
 }
 
-// Добавляем нотифкацию об очистке
+// Добавляем нотификацию об очистке
 function notificationDone() {
   const outTextP = document.querySelector('.outText-1');
   outTextP.classList.add('on_outText-1');
@@ -94,7 +131,7 @@ function getCountryData() {
   generateIban(countryIBANName, countryIBANCount);
 }
 
-// Автоматически генерируем IBAN в зависимости от выбраранного значения в селекторе
+// Автоматически генерируем IBAN в зависимости от выбранного значения в селекторе
 function generateIban(countryCode, lenghtSymbol) {
   let resRandomNumber = '';
   for (let i = 0; i < lenghtSymbol - countryCode.length; i++) {
@@ -116,7 +153,7 @@ function viewGenerateIban(lenghtSymbol, characters) {
   return resCharacters;
 }
 
-//// Функционал кнокпки копирования полученных данных из селектора IBAN
+//// Функционал кнопки копирования полученных данных из селектора IBAN
 // Копируем значением IBAN в буфер обмена
 function doCopy(event) {
   const resultIban = document.querySelector(
@@ -149,7 +186,7 @@ function getPhoneData() {
   generateRandomPhone(phoneCode, phoneValue);
 }
 
-//Функция генерирования случаного номера телефона согласно в формате выбранной страны
+//Функция генерирования случайного номера телефона согласно в формате выбранной страны
 function generateRandomPhone(phoneCode, lengthPhone) {
   let phoneNumber = String();
 
@@ -192,6 +229,104 @@ function copyPhone() {
     });
 }
 
+// Функция открытия/скрытия блока изменения текста
+
+function toggleChangeBlockText() {
+  const changeContainer = document.querySelector('.change-container');
+
+  changeContainer.classList.toggle('active-block');
+
+  // Скрытие блока "Изменить текст" при клике в любое место ВНЕ данного окна
+  document.addEventListener('click', function (event) {
+    if (
+      !changeTextBlock.contains(event.target) &&
+      event.target !== changeTextBlockBtn
+    ) {
+      changeContainer.classList.remove('active-block');
+    }
+  });
+}
+
+// Функция замены части контента в тексте. Функция принимает callback-функцию валидации validChangeText() введённых данных в поля, в которую передаётся массив значений из полей ввода.
+// Если все значения переданы верно - текст в textarea изменяется.
+// Если какие-то данные введены неверно - происходит передача значений из полей в callback-функцию validChangeText() для их валидации и отображения ошибок.
+function changeText(validChangeText) {
+  const [input_1, input_2] = Array.from(inputSearch);
+
+  const searchStr = input_1.value;
+  const changeStr = input_2.value;
+  let text = blockText.value;
+
+  blockText.value = text.replaceAll(searchStr, changeStr);
+  validChangeText([searchStr, changeStr, text]);
+}
+
+// Функция валидация заполненных полей "Найти", "Заменить", "Введите текст"
+function validChangeText(inChangeArray) {
+  let errorBlock = document.querySelector('.error-block-change');
+
+  // Формируем новый объект данных из массива с значениями из полей.
+  const newData = inChangeArray.reduce((acc, el, ind) => {
+    switch (ind) {
+      case 0: {
+        acc.searchStr = el;
+        break;
+      }
+      case 1: {
+        acc.changeStr = el;
+        break;
+      }
+      case 2: {
+        acc.textBlock = el;
+        break;
+      }
+    }
+
+    return acc;
+  }, {});
+
+  if (newData.searchStr.length == 0 || newData.textBlock.length == 0) {
+    errorBlock.innerText =
+      'Поля "Найти", "Текст" не могут быть пустыми! Заполните поля значениями';
+    errorBlock.classList.add('error');
+  } else if (!newData.textBlock.includes(newData.searchStr)) {
+    errorBlock.innerText =
+      'Искомое значение для изменения НЕ НАЙДЕНО в блоке текста';
+    errorBlock.classList.add('error');
+  } else {
+    errorBlock.innerHTML = '';
+    errorBlock.classList.remove('error');
+  }
+}
+
+// Функция копирования изменённого текста из поля "Текст"
+function copyChangeText() {
+  navigator.clipboard
+    .writeText(blockText.value.trim())
+    .then((copyText) => {
+      console.dir(copyText);
+      if (copyText === undefined) {
+        blockText.style.border = '2px solid #2add2a';
+        copyBtn.innerText = 'Успешно ✔';
+      }
+
+      if (blockText.value.length == 0) {
+        blockText.style.border = '2px solid red';
+        copyBtn.innerText = 'Копировать нечего =(';
+        return;
+      }
+
+      setTimeout(() => {
+        copyBtn.innerText = 'Скопировать';
+        blockText.style.border = '2px solid rgba(0, 0, 0, 0.2)';
+      }, 1500);
+    })
+    .catch((err) => {
+      console.log(`Error>>> ${err}`);
+    });
+}
+
+// https:\/\/sky.pro\/media\/kopirovanie-teksta-v-bufer-obmena-s-pomoshhyu-javascript\/
 /*--------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 // function thisPage() {
